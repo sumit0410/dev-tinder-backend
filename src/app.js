9ae540b2d063dcb3acc256ef61fbcaea50ad87e0
Sumit Kumar <sumit@Sumits-MacBook-Air.local>
2026-02-17 01:09:43 +0530

    initial setup

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/test", (req, res) => {
+  res.send("Hello from the test");
+});
+app.use("/hello", (req, res) => {
+  res.send("Hello from the Hello world");
+});
+app.use("/", (req, res) => {
+  res.send("Hello from the Dashboard world");
+});
+
+app.listen(7777, () => {
+  console.log("server is running on port 7777");
+});
