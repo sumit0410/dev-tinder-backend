47145e20ba77962682d29b982dcae760c594f37f
Sumit Kumar <sumit@Sumits-MacBook-Air.local>
2026-02-18 15:50:24 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,17 +2,39 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
+app.use(
+  "/profile",
+  (req, res, next) => {
+    next();
+    // res.send("Profile 1");
+    // console.log("PRofile 1");
+  },
+  (req, res, next) => {
+    res.send("Profile 2");
+    next();
+  },
+  (req, res, next) => {
+    // next();
+    res.send("Profile 3");
+  },
+  (req, res, next) => {
+    // next();
+    res.send("Profile 4");
+  },
+);
+
+app.get("/user/:userId/:name", (req, res) => {
+  console.log(req.params);
   res.send({ firstName: "Sumit", lastName: "Gautam" });
 });
 
-app.post("/user", (req, res) => {
-  res.send("Data has been saved successfully!");
-});
+// app.post("/user", (req, res) => {
+//   res.send("Data has been saved successfully!");
+// });
 
-app.delete("/user", (req, res) => {
-  res.send("Data has been removed from the db");
-});
+// app.delete("/user", (req, res) => {
+//   res.send("Data has been removed from the db");
+// });
 
 app.listen(7777, () => {
   console.log("server is running on port 7777");
